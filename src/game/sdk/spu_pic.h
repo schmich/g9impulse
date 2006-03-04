d2c879401e5d410b0fd6013ac2b3f081c2a8e90d
schmich <schmich@26f3210a-8e49-0410-b8b1-9d8bffe7626b>
2006-03-04 11:28:04 +0000

    Removed PIC18/, renamed PIC16/ to input/, renamed PIC18-new/ to game/

---
+++ src/game/sdk/spu_pic.h
@@ -0,0 +1,15 @@
+//**************************************************************
+//*G9 Impulse SDK - SPU.H
+//*Provides essential SPU functionality for the PIC 16 and PIC18.
+//*
+//*
+//**************************************************************
+
+#ifndef SPU_PIC_H
+#define SPU_PIC_H
+
+#include "common.h"
+
+void playMusic(uint8 track);
+
+#endif
