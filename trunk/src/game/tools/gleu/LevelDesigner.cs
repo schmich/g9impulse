f6468f7483ceba38d4f1d8f125a31c304873d161
schmich <schmich@26f3210a-8e49-0410-b8b1-9d8bffe7626b>
2006-04-02 04:09:09 +0000

    GLEU now using VS.NET-style interface, functionality currently broken.

--- trunk/src/game/tools/gleu/LevelDesigner.cs
+++ trunk/src/game/tools/gleu/LevelDesigner.cs
@@ -3,20 +3,18 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using System.Windows.Forms;
+using WeifenLuo.WinFormsUI;
 
 namespace GLEU
 {
-	/// <summary>
-	/// Summary description for palette.
-	/// </summary>
-	public class Palette : System.Windows.Forms.Form
+	public class LevelDesigner : DockContent
 	{
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
 		private System.ComponentModel.Container components = null;
 
-		public Palette()
+		public LevelDesigner()
 		{
 			//
 			// Required for Windows Form Designer support
@@ -50,20 +48,20 @@ namespace GLEU
 		/// </summary>
 		private void InitializeComponent()
 		{
-            // 
-            // Palette
-            // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(130, 512);
-            this.ControlBox = false;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "Palette";
-            this.ShowInTaskbar = false;
-            this.Text = "Palette";
-
-        }
+			// 
+			// LevelDesigner
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(448, 393);
+			this.BackColor = System.Drawing.Color.Black;
+			this.DockableAreas = ((WeifenLuo.WinFormsUI.DockAreas)(((((WeifenLuo.WinFormsUI.DockAreas.DockLeft | WeifenLuo.WinFormsUI.DockAreas.DockRight) 
+				| WeifenLuo.WinFormsUI.DockAreas.DockTop) 
+				| WeifenLuo.WinFormsUI.DockAreas.DockBottom) 
+				| WeifenLuo.WinFormsUI.DockAreas.Document)));
+			this.DockPadding.Bottom = 3;
+			this.DockPadding.Top = 3;
+			this.Name = "LevelDesigner";
+		}
 		#endregion
-
 	}
 }
