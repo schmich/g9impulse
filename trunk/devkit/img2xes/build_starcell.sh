af2e937bdc123ae16418ab2b3fd0d6d6891c8343
schmich <schmich@26f3210a-8e49-0410-b8b1-9d8bffe7626b>
2006-04-23 06:28:13 +0000

    Added unix2dos step to build_starcell since GXSLOAD chokes on \n only.

--- trunk/devkit/img2xes/build_starcell.sh
+++ trunk/devkit/img2xes/build_starcell.sh
@@ -1,3 +1,11 @@
 #/bin/sh
 
+echo "Generating xes from png..."
+
 perl img2xes.pl -x 320 -y 26214 -d 3+2+3 -pixelwidth 8 -memwidth 16 -address 0x000000 -i starcell_full.png -o starcell.xes
+
+echo "Converting to DOS line endings..."
+
+unix2dos --unix2dos --force starcell.xes
+
+echo "Done."
